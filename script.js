3d6f2f4321521968b3b232c6e256a2bcbb4150c6
Eugenia Shibkova <shibkova91@gmail.com>
2020-03-15 22:36:01 +0300

    feat: add tags switching

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-const navigationBar = document.querySelector('.header__navigation');
+window.onload = () => {
+  onNavigationClick();
+  onTagsClick();
+}
 
-const onClick = ({ target } = event) => {
-  const activeNavLink = document.querySelector('.navigation__item-link--active');
 
-  if (target.classList.contains('navigation__item-link')) {
-      console.log('link')
-    activeNavLink.classList.remove('navigation__item-link--active');
-    target.classList.add('navigation__item-link--active');
-  }
+const onNavigationClick = () => {
+  document.querySelector('.navigation').addEventListener('click', ({target} = event) => {
+    const activeItem = document.querySelector('.navigation__item-link--active');
+
+    if (target.classList.contains('navigation__item-link')) {
+      activeItem.classList.remove('navigation__item-link--active');
+      target.classList.add('navigation__item-link--active');
+    }  
+  });  
 };
 
-document.addEventListener('click', onClick);
+const onTagsClick = () => {
+  const projectsWrapper = document.querySelector('.projects');
+  document.querySelector('.portfolio__tags').addEventListener('click', ({target} = event) => {
+    const activeTag = document.querySelector('.tag--active');
+
+    if (target.classList.contains('tag')) {
+      activeTag.classList.remove('tag--active');
+      target.classList.add('tag--active');
+      
+      const reorderedProjectList = reorderProjectsItems();
+
+      projectsWrapper.innerHTML = '';
+      const template = document.createDocumentFragment();
+ 
+      reorderedProjectList.forEach((element) => {
+        template.appendChild(element);
+      });
+    
+      projectsWrapper.appendChild(template);
+    }  
+  });
+};
+
+const reorderProjectsItems = () => {
+  const projects = document.querySelectorAll('.projects .projects__item');
+
+  const projectsArr = [...projects].reverse();
+  return projectsArr;
+};
